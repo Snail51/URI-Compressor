d4e4f00eec93a6fdb54435d59bb6bab1ea71d7b9
mobren11 <mobren11@gmail.com>
2025-05-17 18:12:42 -0500

    create metrics test script
    script compares the effectiveness of all supported compression algorithms

---
+++ metrics/metricCompression.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Function to generate random string
+generate_random_string()
+{
+    local length=$1
+    local acquire=$(($1 * 8))
+    head -c "$acquire" /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "$length" | head -n 1
+}
+
+generate_from_input()
+{
+    local length=$1
+    cat "./source.txt" | tr -d '\t\n\v\f\r ' | fold -w "$length" | head -n 1
+}
+
+get_length()
+{
+    local cmpr=$1
+    local data=$2
+    echo "$compress_length"
+}
+
+pick_compressor() {
+    array=("LZMA2" "ZLIB")
+    random_element=$(printf "%s\n" "${array[@]}" | shuf -n 1)
+    echo "$random_element"
+}
+
+# Main loop
+
+#LZMA2
+for i in {1..3500}; do
+    random_string=$(generate_from_input "$i")
+
+    #random_compressor=$(pick_compressor)
+    random_compressor="LZMA2"
+
+    result=$(get_length "$random_compressor" "$random_string")
+
+    echo -en "[$i\t/\t3500]\n"
+    echo -en "$i,$random_compressor,$result\n" >> ./result.lzma2.csv
+done
+
+#ZLIB
+for i in {1..3500}; do
+    random_string=$(generate_from_input "$i")
+
+    #random_compressor=$(pick_compressor)
+    random_compressor="ZLIB"
+
+    result=$(get_length "$random_compressor" "$random_string")
+
+    echo -en "[$i\t/\t3500]\n"
+    echo -en "$i,$random_compressor,$result\n" >> ./result.zlib.csv
+done
+
+echo "Test completed."
